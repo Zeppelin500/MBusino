8221a22f245a2f4504d712f93f2b6270ebf4ab49
Zeppelin500 <90451866+Zeppelin500@users.noreply.github.com>
2023-12-12 22:37:12 +0100

    add support of sub unit, tariff and storage number

--- SRC/MBusino/MBusino.ino
+++ SRC/MBusino/MBusino.ino
@@ -272,18 +272,13 @@ void loop() {
       for (uint8_t i=0; i<fields; i++) {
         uint8_t code = root[i]["code"].as<int>();
         const char* name = root[i]["name"];
-        const char* units = root[i]["units"];
-        uint8_t ascii = root[i]["ascii"].as<int>(); //0 = double, 1 = ASCII, 2 = both; 
+        const char* units = root[i]["units"];           
         double value = root[i]["value_scaled"].as<double>(); 
-        const char* valueString = root[i]["value_string"];           
-        
-        //two messages per value
-        if (ascii > 0){
-          client.publish(String(MBUSINO_NAME"/MBus/"+String(i+1)+"_vs_"+String(name)), valueString); // send the value if a ascii value is aviable (variable length)
-        }
-        if(ascii != 1){
-          client.publish(String(MBUSINO_NAME"/MBus/"+String(i+1)+"_"+String(name)), String(value,3).c_str()); // send the value if a real value is aviable (standard)
-        }
+        const char* valueString = root[i]["value_string"];            
+
+        //two messages per value, values comes as number or as ASCII string or both
+        client.publish(String(MBUSINO_NAME"/MBus/"+String(i+1)+"_vs_"+String(name)), valueString); // send the value if a ascii value is aviable (variable length)
+        client.publish(String(MBUSINO_NAME"/MBus/"+String(i+1)+"_"+String(name)), String(value,3).c_str()); // send the value if a real value is aviable (standard)
         client.publish(String(MBUSINO_NAME"/MBus/"+String(i+1)+"_"+String(name)+"_unit"), units);
         //or only one message
         //client.publish(String(MBUSINO_NAME"/MBus/"+String(i+1)+"_"+String(name)+"_in_"+String(units)), String(value,3).c_str());
