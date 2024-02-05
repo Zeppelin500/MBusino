3ea5c4f2731a21374b5884fb79356048082b3ca2
Zeppelin500 <90451866+Zeppelin500@users.noreply.github.com>
2024-02-05 10:45:55 +0100

    Update MBusino_naked_2devices.ino

--- playground/for_uwe/MBusino_naked_2devices/MBusino_naked_2devices.ino
+++ playground/for_uwe/MBusino_naked_2devices/MBusino_naked_2devices.ino
@@ -30,10 +30,12 @@ EspSoftwareSerial::UART ESP8266Serial;
 
 #define MBUS_BAUD_RATE 2400
 #define MBUS_SLAVES 2  // Number of M-Bus Devices at the Bus
+
+#define MBUS_DEVICES 2
 #define MBUS_ADDRESS_1 0x01   // Adress of Slave 1
 #define MBUS_ADDRESS_2 0x02   // Adress of Slave 2
 #define MBUS_TIMEOUT 1000  // milliseconds
-#define MBUS_DATA_SIZE 510
+#define MBUS_DATA_SIZE 254
 #define MBUS_GOOD_FRAME true
 #define MBUS_BAD_FRAME false
 
@@ -48,18 +50,35 @@ EspMQTTClient client(
   1883             // The MQTT port, default to 1883. this line can be omitted
 );
 
-uint8_t mbusAddress[MBUS_SLAVES] = {MBUS_ADDRESS_1,MBUS_ADDRESS_2};
-uint8_t currentAddress = 0;
-
 unsigned long loop_start = 0;
 unsigned long last_loop = 0;
-bool firstrun = true;
 int Startadd = 0x13;  // Start address for decoding
 char jsonstring[6144] = { 0 };
 
-int MbusInterval = 5000;           // interval for MBus request in milliseconds
+uint32_t MbusInterval = 5000;           // interval for MBus request in milliseconds
 
-unsigned long timerMbus = 0;
+//unsigned long timerMbus = 0;
+unsigned long timerMbus1 = 0;
+unsigned long timerMbus2 = 0;
+
+uint8_t currentAddress = 0;
+uint8_t mbusAddress[MBUS_DEVICES] ={MBUS_ADDRESS_1,MBUS_ADDRESS_2};
+uint8_t addressCounter = 0;
+bool mbusCleared = false;
+bool mbusReq = false;
+
+struct settings {
+  char ssid[30];
+  char password[30];
+  char mbusinoName[11];
+  char broker[20];
+  uint16_t mqttPort;
+  uint16_t extension;
+  char mqttUser[30];
+  char mqttPswrd[30]; 
+  uint32_t sensorInterval;
+  uint32_t mbusInterval; 
+} userData = {WLAN_SSID,WLAN_PASSWORD,MBUSINO_NAME,MQTT_Broker,1883,5,"mqttUser","mqttPasword",5000,MbusInterval};
 
 void mbus_request_data(byte address);
 void mbus_short_frame(byte address, byte C_field);
@@ -95,71 +114,88 @@ void onConnectionEstablished() {  // send a message to MQTT broker if connected.
 
 void loop() {
   client.loop();  //MQTT Funktion
-  loop_start = millis();
+
+//------------------------------------------------------------------------------------------------------------
+
+  if(millis() - timerMbus1 > userData.mbusInterval){
+    timerMbus1 = millis();
+    timerMbus2 = millis();
+    currentAddress = mbusAddress[addressCounter];
+    addressCounter++;
+    if(addressCounter == MBUS_DEVICES){
+      addressCounter = 0;
+    }
+    mbus_clearRXbuffer();
+    mbusCleared = true;
+  }
+
+  if(millis() - timerMbus1 > 1000 && mbusCleared == true){
+    mbusReq = true;
+    mbusCleared = false;
+    mbus_request_data(currentAddress);
+  }
+  if(millis() - timerMbus2 > 2000 && mbusReq == true){
+    mbusReq = false;
+    bool mbus_good_frame = false;
+    byte mbus_data[MBUS_DATA_SIZE] = { 0 };
+    mbus_good_frame = mbus_get_response(mbus_data, sizeof(mbus_data));
+
+    /*
+    //------------------ only for debug, you will recieve the whole M-Bus telegram bytewise in HEX for analysis -----------------
+    for(uint8_t i = 0; i <= mbus_data[1]+1; i++){                                                             //|
+      char buffer[3];                                                                                         //|
+      sprintf(buffer,"%02X",mbus_data[i]);                                                                    //|
+      client.publish(String(String(userData.mbusinoName) + "/debug/telegram_byte_"+String(i)), String(buffer).c_str());          //|
+    }                                                                                                         //|
+    //--------------------------------------------------------------------------------------------------------------------------    
+    */
+    //mbus_good_frame = true;
+    //byte mbus_data2[] = {0x68,0xC1,0xC1,0x68,0x08,0x00,0x72,0x09,0x34,0x75,0x73,0xC5,0x14,0x00,0x0D,0x43,0x00,0x00,0x00,0x04,0x78,0x41,0x63,0x65,0x04,0x04,0x06,0xAA,0x29,0x00,0x00,0x04,0x13,0x40,0xA1,0x75,0x00,0x04,0x2B,0x00,0x00,0x00,0x00,0x14,0x2B,0x3C,0xF3,0x00,0x00,0x04,0x3B,0x48,0x06,0x00,0x00,0x14,0x3B,0x4E,0x0E,0x00,0x00,0x02,0x5B,0x19,0x00,0x02,0x5F,0x19,0x00,0x02,0x61,0xFA,0xFF,0x02,0x23,0xAC,0x08,0x04,0x6D,0x03,0x2A,0xF1,0x2A,0x44,0x06,0x92,0x0C,0x00,0x00,0x44,0x13,0x2D,0x9B,0x1C,0x00,0x42,0x6C,0xDF,0x2C,0x01,0xFD,0x17,0x00,0x03,0xFD,0x0C,0x05,0x00,0x00,0x84,0x10,0x06,0x1A,0x00,0x00,0x00,0xC4,0x10,0x06,0x05,0x00,0x00,0x00,0x84,0x20,0x06,0x00,0x00,0x00,0x00,0xC4,0x20,0x06,0x00,0x00,0x00,0x00,0x84,0x30,0x06,0x00,0x00,0x00,0x00,0xC4,0x30,0x06,0x00,0x00,0x00,0x00,0x84,0x40,0x13,0x00,0x00,0x00,0x00,0xC4,0x40,0x13,0x00,0x00,0x00,0x00,0x84,0x80,0x40,0x13,0x00,0x00,0x00,0x00,0xC4,0x80,0x40,0x13,0x00,0x00,0x00,0x00,0x84,0xC0,0x40,0x13,0x00,0x00,0x00,0x00,0xC4,0xC0,0x40,0x13,0x00,0x00,0x00,0x00,0x75,0x16};
 
 
-  if ((loop_start-last_loop)>= MbusInterval || firstrun) { 
-    last_loop = loop_start; firstrun = false;
-    for(uint8_t k=0;k<MBUS_SLAVES;k++){
-      bool mbus_good_frame = false;
-      byte mbus_data[MBUS_DATA_SIZE] = { 0 };
-      currentAddress = mbusAddress[k];
-      mbus_clearRXbuffer();
-      mbus_request_data(mbusAddress[k]);
-      mbus_good_frame = mbus_get_response(mbus_data, sizeof(mbus_data));
-      /*
-      //------------------ only for debug, you will recieve the whole M-Bus telegram bytewise in HEX for analysis -----------------
-      for(uint8_t i = 0; i <= mbus_data[1]+1; i++){                                                             //|
-        char buffer[3];                                                                                         //|
-        sprintf(buffer,"%02X",mbus_data[i]);                                                                    //|
-        client.publish(String(MBUSINO_NAME"/debug/telegram_byte_"+String(i)), String(buffer).c_str());          //|
-      }                                                                                                         //|
-      //--------------------------------------------------------------------------------------------------------------------------    
-      */
-      if (mbus_good_frame) {
-        int packet_size = mbus_data[1] + 6; 
-        MBusinoLib payload(255);  
-        DynamicJsonDocument jsonBuffer(4080);
-        JsonArray root = jsonBuffer.createNestedArray();  
-        uint8_t fields = payload.decode(&mbus_data[Startadd], packet_size - Startadd - 2, root); 
-        
-        serializeJsonPretty(root, jsonstring);
-        client.publish(MBUSINO_NAME"_Slave"+String(k+1)+"/MBus/error", String(payload.getError()));  // kann auskommentiert werden wenn es läuft
-        client.publish(MBUSINO_NAME"_Slave"+String(k+1)+"/MBus/jsonstring", String(jsonstring));
+    if (mbus_good_frame) {
+      int packet_size = mbus_data[1] + 6; 
+      MBusinoLib payload(254);  
+      JsonDocument jsonBuffer;
+      JsonArray root = jsonBuffer.add<JsonArray>();  
+      uint8_t fields = payload.decode(&mbus_data[Startadd], packet_size - Startadd - 2, root); 
+      char jsonstring[2048] = { 0 };
+      yield();
+      serializeJson(root, jsonstring);
+      client.publish(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBus/error", String(payload.getError()));  // kann auskommentiert werden wenn es läuft
+      client.publish(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBus/jsonstring", String(jsonstring));
 
 
-        for (uint8_t i=0; i<fields; i++) {
-          uint8_t code = root[i]["code"].as<int>();
-          const char* name = root[i]["name"];
-          const char* units = root[i]["units"];           
-          double value = root[i]["value_scaled"].as<double>(); 
-          const char* valueString = root[i]["value_string"];            
+      for (uint8_t i=0; i<fields; i++) {
+        uint8_t code = root[i]["code"].as<int>();
+        const char* name = root[i]["name"];
+        const char* units = root[i]["units"];           
+        double value = root[i]["value_scaled"].as<double>(); 
+        const char* valueString = root[i]["value_string"];            
 
-          //two messages per value, values comes as number or as ASCII string or both
-          client.publish(String(MBUSINO_NAME"_Slave"+String(k+1)+"/MBus/"+String(i+1)+"_vs_"+String(name)), valueString); // send the value if a ascii value is aviable (variable length)
-          client.publish(String(MBUSINO_NAME"_Slave"+String(k+1)+"/MBus/"+String(i+1)+"_"+String(name)), String(value,3).c_str()); // send the value if a real value is aviable (standard)
-          client.publish(String(MBUSINO_NAME"_Slave"+String(k+1)+"/MBus/"+String(i+1)+"_"+String(name)+"_unit"), units);
-          //or only one message
-          //client.publish(String(MBUSINO_NAME"_Slave"+String(k+1)+"/MBus/"+String(i+1)+"_"+String(name)+"_in_"+String(units)), String(value,3).c_str());
+        //two messages per value, values comes as number or as ASCII string or both
+        client.publish(String(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBus/"+String(i+1)+"_vs_"+String(name)), valueString); // send the value if a ascii value is aviable (variable length)
+        client.publish(String(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBus/"+String(i+1)+"_"+String(name)), String(value,3).c_str()); // send the value if a real value is aviable (standard)
+        client.publish(String(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBus/"+String(i+1)+"_"+String(name)+"_unit"), units);
+        //or only one message
+        //client.publish(String(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBus/"+String(i+1)+"_"+String(name)+"_in_"+String(units)), String(value,3).c_str());
 
-
-          if (i == 3){  // Sensostar Bugfix --> comment it out if you use not a Sensostar
-            float flow = root[5]["value_scaled"].as<float>();
-            float delta = root[9]["value_scaled"].as<float>();
-            float calc_power = delta * flow * 1163;          
-            client.publish(MBUSINO_NAME"_Slave"+String(k+1)+"/MBus/4_power_calc", String(calc_power).c_str());           
-          }  
-        
-        }
-    
-      } 
-      else {
-    //Fehlermeldung
-          client.publish(String(MBUSINO_NAME"_Slave"+String(k+1)+"/MBUSerror"), "no_good_telegram");
+        if (i == 3){  // Sensostar Bugfix --> comment it out if you use not a Sensostar
+          float flow = root[5]["value_scaled"].as<float>();
+          float delta = root[9]["value_scaled"].as<float>();
+          float calc_power = delta * flow * 1163;          
+          client.publish(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBus/4_power_calc", String(calc_power).c_str());           
+        }   
+        yield();      
       }
+    } 
+    else {
+  //Fehlermeldung
+        client.publish(String(MBUSINO_NAME"_Slave"+String(addressCounter+1)+"/MBUSerror"), "no_good_telegram");
+
     }
     mbus_normalize(currentAddress);
-  } // ende - interner vermerk
+  }
 }
 
 void mbus_request_data(byte address) {
